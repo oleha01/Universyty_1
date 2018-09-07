dded7c5a20c7c8c145a91d67764b9c718939b85f
Олег Бутрий <oleha01@butryy.onmicrosoft.com>
2018-09-07 18:55:17 +0300

    Unit Test for class PhoneContact

--- UnitTestForTask1/UnitTest1.cs
+++ UnitTestForTask1/UnitTest1.cs
@@ -1,15 +1,23 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-
+using System.IO;
+using Task1;
 namespace UnitTestForTask1
 {
     [TestClass]
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestMethodForClassPhoneContact()
         {
-            Assert.AreEqual(0, 0);
+            PhoneContact phone = new PhoneContact("Roman", "+3801234567");
+            StreamWriter sw = new StreamWriter("FileForTest.txt");
+            phone.WrtiteToFile(sw);
+            sw.Close();
+            StreamReader sr = new StreamReader("FileForTest.txt");
+            PhoneContact phone1 = new PhoneContact();
+            phone1.ReadFromFile(sr);
+            Assert.IsTrue(phone1.name == "Roman," && phone1.number == "+3801234567" && phone.name == "Roman" && phone.number == "+3801234567");
         }
     }
 }
