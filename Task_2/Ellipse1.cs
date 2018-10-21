aecf188c0647c65640615698e661ef13d2f83d3e
Roman <42993647+romankoltun@users.noreply.github.com>
2018-10-21 22:10:11 +0300

    Update Ellipse1.cs

--- Task_2/Ellipse1.cs
+++ Task_2/Ellipse1.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Collections.Generic;
 using System.IO;
 using System.Windows;
 using System.Windows.Controls;
@@ -12,25 +11,39 @@ using System.Windows.Media;
 using System.Windows.Shapes;
 using Microsoft.Win32;
 
-namespace Task_2
+namespace WpfApp1
 {
     [Serializable]
    public class Ellipse1
     {
+        public Ellipse1()
+        {
+            Name = "";
+        }
+        public Ellipse1(string s)
+        {
+            Name = s;
+        }
         public double Height { get; set; }
         public double Width { get; set; }
+        public string Name { get; set; }
         public double LeftProperty { get; set; }
         public double TopProperty { get; set; }
         public string br { get; set; }
+        public byte Red { get; set; }
+        public byte Green { get; set; }
+        public byte Blue { get; set; }
+        public byte A { get; set; }
         public Ellipse Draw()
         {
             Ellipse elipse = new Ellipse();
-            
             elipse.Stroke = Brushes.Black;
+            elipse.StrokeThickness = 5;
             elipse.SetValue(Canvas.LeftProperty, LeftProperty);
             elipse.SetValue(Canvas.TopProperty, TopProperty);
             elipse.Height = Height;
             elipse.Width = Width;
+            elipse.Fill = new SolidColorBrush(Color.FromArgb(A, Red, Green, Blue));
             return elipse;
         }
 
