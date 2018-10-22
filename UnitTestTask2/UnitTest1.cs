773b312f4713b4be0fba8190ed5905fbdd502d78
Олег Бутрий <oleha01@butryy.onmicrosoft.com>
2018-10-22 21:06:27 +0300

    /

--- UnitTestTask2/UnitTest1.cs
+++ UnitTestTask2/UnitTest1.cs
@@ -11,17 +11,17 @@ namespace UnitTestTask2
     public class UnitTest1
     {
         [TestMethod]
-        //public void DrawTest()
-        //{
-        //    Ellipse elipse = new Ellipse();
-        //    elipse.Stroke = Brushes.Black;
-        //    elipse.StrokeThickness = 5;
-        //    elipse.SetValue(Canvas.LeftProperty, this.LeftProperty);
-        //    elipse.SetValue(Canvas.TopProperty, this.TopProperty);
-        //    elipse.Height = this.Height;
-        //    elipse.Width = this.Width;
-        //    elipse.Fill = new SolidColorBrush(Color.FromArgb(this.A, this.Red, this.Green, this.Blue));
-        //    Assert.Fail();
-        //}
+        public void DrawTest()
+        {
+            Ellipse elipse = new Ellipse();
+            elipse.Stroke = Brushes.Black;
+            elipse.StrokeThickness = 5;
+            elipse.SetValue(Canvas.LeftProperty, this.LeftProperty);
+            elipse.SetValue(Canvas.TopProperty, this.TopProperty);
+            elipse.Height = this.Height;
+            elipse.Width = this.Width;
+            elipse.Fill = new SolidColorBrush(Color.FromArgb(this.A, this.Red, this.Green, this.Blue));
+            Assert.Fail();
+        }
     }
 }
