f084414f7ed49f0430f89cc14b186b8b0e9075fd
Burdeina Iruna <iruna.burdeina@gmail.com>
2018-11-11 18:32:13 +0200

    Fix class Driver

--- PlatformTask_3/Logic/Driver.cs
+++ PlatformTask_3/Logic/Driver.cs
@@ -8,12 +8,18 @@ namespace Logic
 {
     public class Driver
     {
-        private bool IsFinalAddress = false;
+        
         CarClass carcl { get; set; }
-        public Address Address1 { get; set; }
-        private Address address2;
-        public Address Address2 { get { return address2; } set { address2 = value; IsFinalAddress = true; } }
+        
         public string Name
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set { }
+        }
+        public string SurName
         {
             get
             {
@@ -29,27 +35,24 @@ namespace Logic
             }
             set { }
         }
-        public Driver(string n, string id, string city1, string street1, string house1,  string entrance1 = "",  CarClass car = CarClass.Econom)
+        public Driver(string n, string id, string sur, CarClass car = CarClass.Econom)
         {
             Name = n;
             Id = id;
-            Address1 = new Address(city1, street1, house1, entrance1);
-            
+            SurName=sur;
             carcl = car;
         }
-        public Driver(string n,string id, string city1, string street1, string house1, string city2, string street2, string house2, string entrance1 = "", string entrance2 = "", CarClass car = CarClass.Econom)
+        public Driver()
         {
-            Name = n;
-            Id = id;
-            Address1 = new Address(city1, street1, house1, entrance1);
-            Address2 = new Address(city2, street2, house2, entrance2);
-            carcl = car;
+            Name = " ";
+            Id = " ";
+            SurName=" ";
+            carcl = CarClass.Universal;
         }
         public override string ToString()
         {
-            if (IsFinalAddress)
-                return String.Format("Name: {0}, With: {1} to {2}, ClassCar: {3};", Name,  Address1, Address2, carcl);
-            return String.Format("Name: {0}, With: {1}, ClassCar: {2} ;", Name, Address1, Address2, carcl );
+            
+            return String.Format("Name: {0}, SurName: {1}, ClassCar: {2} ;", Name, SurName, carcl );
 
         }
 
