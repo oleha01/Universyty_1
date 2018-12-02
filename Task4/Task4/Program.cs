56cded61fa2c9235c12cef7814316623280f5997
Burdeina <32615304+Burdeina@users.noreply.github.com>
2018-12-02 19:37:55 +0200

    test Northwind

--- Task4/Task4/Program.cs
+++ Task4/Task4/Program.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System;
+using System.Configuration;
+using System.Data.SqlClient;
 
 namespace Task4
 {
@@ -10,6 +8,19 @@ namespace Task4
     {
         static void Main(string[] args)
         {
+            string connectionString = ConfigurationManager.ConnectionStrings["NorthwindConnectionString"].ConnectionString;
+            SqlConnection connection = new SqlConnection(connectionString);
+            connection.Open();
+            Console.WriteLine("All last names of the employees in Northwind");
+            SqlCommand command = connection.CreateCommand();
+            command.CommandText = "SELECT LastName FROM Employees;";
+            SqlDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                Console.WriteLine("{0}", reader["LastName"]);
+            }
+            Console.ReadKey();
+            reader.Close();
         }
     }
 }
