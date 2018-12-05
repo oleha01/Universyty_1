82a8a05e5dd03a561ceab7f2879835c67c1accc9
Antonina Ivanova <38051700+LausDio@users.noreply.github.com>
2018-12-06 00:50:34 +0200

    StyleCop

--- Task4/Task4/Program.cs
+++ Task4/Task4/Program.cs
@@ -1,25 +1,42 @@
- using System;
-using System.Configuration;
-using System.Data.SqlClient;
+//-----------------------------------------------------------------------
+// <copyright file="Program.cs" company="LNU">
+//     Copyright (c) Top Coders. All rights reserved.
+// </copyright>
+// <author>Burdein Irina</author>
+// <author>Butry Oleg</author>
+// <author>Ivanova Antonina</author>
+// <author>Koltun Roman</author>
+// <date> " + DateTime.Now + @"</date>
+//-----------------------------------------------------------------------
 
 namespace Task4
 {
-    class Program
-    {
-        public delegate void Que();
-        static void Main(string[] args)
-        {
+    using System;
+    using System.Configuration;
 
+    /// <summary>
+    /// Main program.
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// Delegate for queries.
+        /// </summary>
+        public delegate void Que();
+
+        /// <summary>
+        /// The entry point of program.
+        /// </summary>
+        /// <param name="args">Collection of Strings, separated by a space, which can be typed into the program on the terminal.</param>
+        public static void Main(string[] args)
+        {
             Queries que = new Queries(ConfigurationManager.ConnectionStrings["NorthwindConnectionString"].ConnectionString);
 
             que.RunAll();
-          //  que.Q34();
+            ////que.Q34();
           
              que.connection.Close();
             Console.ReadKey();
-           
-
         }
     }
-  
 }
