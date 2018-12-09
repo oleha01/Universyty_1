b88d016a50640ac4b370c61eb2b5369fdf158a7c
Burdeina Iruna <iruna.burdeina@gmail.com>
2018-12-09 22:03:48 +0200

    Add unitTest for Adress

---
+++ PlatformTask_3/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Logic;
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+        [TestMethod]
+        public void CityTest()
+        {
+            string expectedCity = "Lviv";
+            Address route = new Address("Lviv", "Lybinska", "14", "2");
+
+            Assert.IsTrue(route.City == expectedCity);
+        }
+
+        [TestMethod]
+        public void StreetTest()
+        {
+            string expectedStreet = "Lybinska";
+            Address route = new Address("Lviv", "Lybinska", "14", "2");
+
+            Assert.IsTrue(route.Street == expectedStreet);
+        }
+        [TestMethod]
+        public void HouseTest()
+        {
+            string expectedHouse = "14";
+            Address route = new Address("Lviv", "Lybinska", "14", "2");
+
+            Assert.IsTrue(route.House == expectedHouse);
+        }
+        [TestMethod]
+        public void EntranceTest()
+        {
+            string expectedEntrance = "2";
+            Address route = new Address("Lviv", "Lybinska", "14", "2");
+
+            Assert.IsTrue(route.Entrance == expectedEntrance);
+        }
+    }
+}
